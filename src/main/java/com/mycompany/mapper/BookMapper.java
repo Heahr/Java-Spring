ca0f5257df1627b515f21100dd8160deb27487fc
Heahr <bvnm1994@naver.com>
2017-07-14 12:29:40 +0900

    Spring-project 3

---
+++ src/main/java/com/mycompany/mapper/BookMapper.java
@@ -0,0 +1,7 @@
+package com.mycompany.mapper;
+import org.apache.ibatis.annotations.Insert;
+import com.mycompany.vo.Book;
+public interface BookMapper {
+    @Insert("insert into books (title, author, image) values (#{title}, #{author}, #{image})")
+    public boolean create(Book book);
+}
